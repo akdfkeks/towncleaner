3904fa20b12a4258fd27f8d2ff6356004f7f269a
akdfkeks <akdfkeks@gmail.com>
2022-09-26 20:43:54 +0900

    .

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,14 @@
 import express from "express";
 import config from "./config";
 import router from "./api";
+import cors from "cors";
 
 async function createServer() {
 	const app = express();
+
+	app.use(cors());
+	app.use(express.json());
+	app.use(express.urlencoded({ extended: false }));
 
 	app.use(router());
 
