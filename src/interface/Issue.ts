d050e9dae7e918719dc0870ca1f41ab7eddcc02b
akdfkeks <akdfkeks@gmail.com>
2022-10-03 15:28:46 +0900

    change: var name

--- src/interface/Issue.ts
+++ src/interface/Issue.ts
@@ -1,14 +1,22 @@
-export interface IssueListRequest {
-	user_latitude: string;
-	user_longitude: string;
+export interface UserLatlng {
+	lat: string;
+	lng: string;
 }
 
-export interface AddIssueRequest extends IssueListRequest {}
+export interface AddIssueRequest extends UserLatlng {}
 
+/**
+ * 유저에게 반환될 이슈는 다음의 요소를 가져야함
+ * 1. 제목
+ * 2. 내용
+ * 3. 사진
+ * 4. 클래스
+ * 5. 위치(위도, 경도)
+ */
 export interface IssuePoint {
 	title: string;
 	body: string;
 	class: string;
-	latitude: string;
-	longitude: string;
+	location: UserLatlng;
+	img: string;
 }
