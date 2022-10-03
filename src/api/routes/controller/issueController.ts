244efaa126c5d8827812a7c1bae0a8b3cb7eee64
akdfkeks <akdfkeks@gmail.com>
2022-10-03 15:26:29 +0900

    change: variable names and add info controller

--- src/api/routes/controller/issueController.ts
+++ src/api/routes/controller/issueController.ts
@@ -1,7 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 import IssueService from "@/service/IssueService";
 import Container from "typedi";
-import { IssueListRequest } from "@/interface/Issue";
+import { UserLatlng } from "@/interface/Issue";
 
 export default {
 	async fixedPoint(req: Request, res: Response) {
@@ -16,13 +16,13 @@ export default {
 	},
 
 	async userPoint(req: Request, res: Response) {
-		const userPointData: IssueListRequest = {
-			user_latitude: req.body.issueRequest.user_latitude,
-			user_longitude: req.body.issueRequest.user_longitude,
+		const userPointData: UserLatlng = {
+			lat: req.body.issueRequest.user_latitude,
+			lng: req.body.issueRequest.user_longitude,
 		};
 
 		const IssueServiceInstance = Container.get(IssueService);
-		const data = await IssueServiceInstance.getUserPointIssues(userPointData);
+		const { data } = await IssueServiceInstance.getUserPointIssues(userPointData);
 
 		res.status(200).json({
 			success: true,
@@ -33,6 +33,14 @@ export default {
 
 	async issueInfo(req: Request, res: Response) {
 		const { issueNo } = req.params;
+		const IssueServiceInstance = Container.get(IssueService);
+		const { data } = await IssueServiceInstance.getIssueInfo(issueNo);
+
+		res.status(200).json({
+			success: true,
+			message: "List of issues around the user point",
+			data: data, // TODO:
+		});
 	},
 	async solveIssue(req: Request, res: Response) {},
 };
