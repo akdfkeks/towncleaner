4a67bb7d6e579179649338539eed8c2ecb275e72
akdfkeks <akdfkeks@gmail.com>
2022-10-03 15:23:54 +0900

    change : move express loading to loader

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,13 @@
 import express from "express";
 import config from "./config";
 import router from "./api";
-import cors from "cors";
+import appLoader from "./loader";
 import "reflect-metadata";
 
 async function createServer() {
 	const app = express();
 
-	app.use(cors());
-	app.use(express.json());
-	app.use(express.urlencoded({ extended: false }));
-
-	app.use(router());
+	await appLoader({ app });
 
 	app.listen(config.port, () => {
 		console.log(`Server listening on http://${config.host}:${config.port}`);
