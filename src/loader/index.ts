15cdf6aaf33082cf3b8bf5ad60cb42652b533d1d
akdfkeks <akdfkeks@gmail.com>
2022-10-03 16:46:12 +0900

    .

--- src/loader/index.ts
+++ src/loader/index.ts
@@ -1,7 +1,8 @@
 import expressLoader from "./expressLoader";
 import depsInjectionLoader from "./depsInjectionLoader";
+import { Application } from "express";
 
-async function appLoader({ app }) {
+async function appLoader({ app }: { app: Application }) {
 	// 1. DI, IoC Object 초기화
 	await depsInjectionLoader();
 
