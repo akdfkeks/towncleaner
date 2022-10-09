fec7dd15d6aeeee950a715ef77b0a2e21b970222
akdfkeks <akdfkeks@gmail.com>
2022-10-09 20:07:13 +0900

    .

--- src/api/index.ts
+++ src/api/index.ts
@@ -4,10 +4,12 @@ import issue from "./routes/issue";
 import quest from "./routes/quest";
 import error from "./routes/error";
 import test from "./routes/test";
+import log from "./routes/log";
 
 export default () => {
 	const app = Router();
 
+	log(app);
 	auth(app);
 	issue(app);
 	quest(app);
