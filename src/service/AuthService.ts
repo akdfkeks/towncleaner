dcae0ac34afe9772369d79020ba82e3668b7775c
akdfkeks <akdfkeks@gmail.com>
2022-10-09 20:04:13 +0900

    change: interface name

--- src/service/AuthService.ts
+++ src/service/AuthService.ts
@@ -1,7 +1,7 @@
 import { Inject, Service } from "typedi";
 import AuthModel from "../model/AuthModel";
-import { SignUpForm } from "../interface/Auth";
-import { LoginForm, LoginResult } from "../interface/Auth";
+import { SignUpRequest } from "../interface/Auth";
+import { LoginRequest, ResponseMessage } from "../interface/Auth";
 import jwt from "jsonwebtoken";
 import argon2 from "argon2";
 
@@ -9,14 +9,14 @@ import argon2 from "argon2";
 class AuthService {
 	@Inject("AuthModel") private authModel: AuthModel;
 
-	public async signUp(user: SignUpForm) {
+	public async signUp(user: SignUpRequest) {
 		// 아이디 사용 여부 확인
 		const userExist = await this.authModel.getUserExist(user);
 		if (userExist) throw new Error("User already Exist");
 
 		// 비밀번호 해싱 및 DB 저장할 데이터 형식 생성
 		const hashedPw = await argon2.hash(user.pw, { type: argon2.argon2i });
-		const signUpData: SignUpForm = {
+		const signUpData: SignUpRequest = {
 			id: user.id,
 			pw: hashedPw,
 		};
@@ -28,13 +28,13 @@ class AuthService {
 		return { signUpResult };
 	}
 
-	async localLogin(loginRequest: LoginForm) {
+	async localLogin(loginRequest: LoginRequest) {
 		const userData = await this.authModel.getUserData(loginRequest);
 		if (!userData) {
 			throw new Error("User not Exist");
 		}
 
-		const pwVerification = await argon2.verify(userData.pw, loginRequest.userPw, {
+		const pwVerification = await argon2.verify(userData.pw, loginRequest.id, {
 			type: argon2.argon2i,
 		});
 		if (!pwVerification) {
