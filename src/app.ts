1659f48345ea9e78f1991ac540cbd6c34132fb91
akdfkeks <akdfkeks@gmail.com>
2022-10-17 20:10:27 +0900

    add: support https

--- src/app.ts
+++ src/app.ts
@@ -3,14 +3,26 @@ import config from "./config";
 import router from "./api";
 import appLoader from "./loader";
 import "reflect-metadata";
+import fs from "fs";
+import https from "https";
+import http from "http";
+
+const options = {
+	ca: fs.readFileSync("/etc/letsencrypt/live/valun.kro.kr/fullchain.pem"),
+	key: fs.readFileSync("/etc/letsencrypt/live/valun.kro.kr/privkey.pem"),
+	cert: fs.readFileSync("/etc/letsencrypt/live/valun.kro.kr/cert.pem"),
+};
 
 async function createServer() {
 	const app = express();
 
 	await appLoader({ app });
 
-	app.listen(config.port, () => {
+	http.createServer(app).listen(config.port, () => {
 		console.log(`Server listening on http://${config.host}:${config.port}`);
+	});
+	https.createServer(options, app).listen(443, () => {
+		console.log(`Server listening on https://${config.host}:${config.port}`);
 	});
 }
 
