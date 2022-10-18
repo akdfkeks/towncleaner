82582ead5992072f71d174a11340166261c1e187
akdfkeks <akdfkeks@gmail.com>
2022-10-18 11:10:21 +0900

    .

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,9 @@ import config from "./config";
 import appLoader from "./loader";
 import https from "https";
 import http from "http";
-import dotenv from "dotenv";
 import { httpsOptions } from "./config/ssl";
 
 async function createServer() {
-	dotenv.config();
-
 	const app = express();
 
 	await appLoader({ app });
