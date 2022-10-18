2e6d21763973ee50856d5bb61f37f07ab8b4b23a
akdfkeks <akdfkeks@gmail.com>
2022-10-18 11:12:17 +0900

    .

--- src/config/ssl.ts
+++ src/config/ssl.ts
@@ -2,12 +2,16 @@ import fs from "fs";
 import config from ".";
 import https from "https";
 import os from "os";
+import dotenv from "dotenv";
+
+dotenv.config();
+
 const isProd = os.platform() == "darwin" ? false : true;
 
 export const httpsOptions: https.ServerOptions | null = isProd
 	? {
-			ca: fs.readFileSync(config.fullchain),
-			key: fs.readFileSync(config.privkey),
-			cert: fs.readFileSync(config.cert),
+			ca: fs.readFileSync(process.env.FULLCAIN as string),
+			key: fs.readFileSync(process.env.PRIVKEY as string),
+			cert: fs.readFileSync(process.env.CERT as string),
 	  }
 	: null;
