23b18192f59b2e69874bc664848d81c63c2ea0de
akdfkeks <akdfkeks@gmail.com>
2022-10-22 19:51:02 +0900

    add: Error Factory 에서 핸들링할 에러를 추가하였습니다

--- src/function/errorTypeChecker.ts
+++ src/function/errorTypeChecker.ts
@@ -1,9 +1,16 @@
 import { PrismaClientKnownRequestError, PrismaClientValidationError } from "@prisma/client/runtime";
-import { InvalidDataError, PrismaClientError } from "../error/Error";
+import {
+	AbstractExpectedError,
+	InvalidDataError,
+	IssueImageExifError,
+	PrismaClientError,
+	UnExpectedError,
+} from "../error/Error";
 
 export function errorFactory(err: Error): Error {
 	if (err instanceof TypeError) return new InvalidDataError(err);
 	if (err instanceof PrismaClientValidationError) return new InvalidDataError(err);
 	if (err instanceof PrismaClientKnownRequestError) return new PrismaClientError(err);
-	return err;
+	if (err instanceof AbstractExpectedError) return err;
+	return new UnExpectedError(err);
 }
