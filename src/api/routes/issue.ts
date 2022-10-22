ccb7fd97160d07c6f55069f47010157d486a73a8
akdfkeks <akdfkeks@gmail.com>
2022-10-22 19:46:48 +0900

    .

--- src/api/routes/issue.ts
+++ src/api/routes/issue.ts
@@ -9,12 +9,10 @@ export default (app: Router) => {
 	app.use("/issue", route);
 
 	route.use(function (req, res, next) {
-		//console.log(req.ip);
 		next();
 	});
 
 	route.post("/create", devAuth, saveFileToLocal.single("image"), issueController.createIssue);
-	//route.post("/create", devAuth, saveFile, issueController.createIssue);
 
 	route.post("/:issueNo/solve", devAuth, issueController.solveIssue);
 
@@ -26,8 +24,3 @@ export default (app: Router) => {
 
 	return app;
 };
-
-/**
- * 이슈는 생성 조회 수정 삭제 해결 이 가능하다
- * , IssueImageUploader.single("img")
- */
