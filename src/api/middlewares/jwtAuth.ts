33eb82883b51a92fd51c5f489411a8a0c6d6878f
akdfkeks <akdfkeks@gmail.com>
2022-10-29 12:30:59 +0900

    .

--- src/api/middlewares/jwtAuth.ts
+++ src/api/middlewares/jwtAuth.ts
@@ -2,14 +2,14 @@ import { NextFunction, Request, Response } from "express";
 import { JwtUserPayload, LoginRequestBody } from "../../interface/Auth";
 import jwt from "jsonwebtoken";
 import config from "../../config";
-import { AbstractExpectedError, AuthenticationError } from "../../error/Error";
+import { PermissionError, AuthenticationError } from "../../error/Error";
 
 export function jwtAuth(req: Request, res: Response, next: NextFunction) {
-	// 토큰에 쿠키가 없다면 Error
-	if (!req.signedCookies.valun) throw new AuthenticationError("로그인이 필요한 기능입니다.");
+	if (!req.signedCookies.valun) throw new PermissionError("로그인이 필요한 기능입니다.");
 
 	// 토큰 추출 및 검증
 	const userToken = req.signedCookies.valun;
+
 	try {
 		const decoded = jwt.verify(userToken, config.JWT_SECRET) as JwtUserPayload;
 		req.reqUser = decoded;
@@ -19,14 +19,13 @@ export function jwtAuth(req: Request, res: Response, next: NextFunction) {
 			next(new AuthenticationError("토큰이 만료되었습니다."));
 		if (err instanceof jwt.NotBeforeError)
 			next(new AuthenticationError("유효하지 않은 토큰입니다."));
-		if (err instanceof jwt.JsonWebTokenError) next(new AuthenticationError("JWT 인증 오류"));
-		if (err instanceof AbstractExpectedError) next(err);
+		if (err instanceof jwt.JsonWebTokenError) next(new AuthenticationError(err));
 		next(err);
 	}
 }
 
 // ------------------------isDev------------------------
-export const devAuth = (req: Request, res: Response, next: NextFunction) => {
+export const devJwtAuth = (req: Request, res: Response, next: NextFunction) => {
 	if (config.isDev) {
 		req.reqUser = { id: "test1", name: "dev" };
 	}
