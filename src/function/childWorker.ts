3ca864d2b483fcf76e00cd9cc45fc126fe5adb7c
akdfkeks <akdfkeks@gmail.com>
2022-10-30 00:46:04 +0900

    change: Promise.all 을 통한 데이터 업데이트

--- src/function/childWorker.ts
+++ src/function/childWorker.ts
@@ -16,21 +16,24 @@ export async function detectObject(fileName: string, issueId: number, imageId: n
 				if (subClass <= 19) return 3;
 				if (subClass <= 24) return 4;
 			})();
-			const result = await prisma.issue.update({
-				where: { id: issueId },
-				data: {
-					class: imageClass,
-				},
-			});
-			const result2 = await prisma.issue_image.update({
-				where: { id: imageId },
-				data: {
-					// Detected_obj : {
-					// 	connectOrCreate : {
-					// 	}
-					// }
-				},
-			});
+
+			const result = await Promise.all([
+				await prisma.issue.update({
+					where: { id: issueId },
+					data: {
+						class: imageClass,
+					},
+				}),
+				await prisma.issue_image.update({
+					where: { id: imageId },
+					data: {
+						// Detected_obj : {
+						// 	connectOrCreate : {
+						// 	}
+						// }
+					},
+				}),
+			]);
 
 			// If process done
 			child.kill();
